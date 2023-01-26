1b6d96e2428832f67ae99838ce7fec2ce1c4709d
RazraB0tchik <100565729+RazraB0tchik@users.noreply.github.com>
2023-01-26 21:36:26 +1000

    final commit

--- src/main/java/encryption/main/encryption/EncryptionMain.java
+++ src/main/java/encryption/main/encryption/EncryptionMain.java
@@ -9,7 +9,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Objects;
 
 public class EncryptionMain extends Application {
@@ -19,8 +18,8 @@ public class EncryptionMain extends Application {
         Image image = new Image(icon);
         stage.getIcons().add(image);
         stage.setTitle("Menu");
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Menu.fxml"))); //FXMLLoader создает интерфейс из файла, при помощи load выгружает объект типа Parent
-        stage.setScene(new Scene(root)); //передаем в нашу сцену наш извлеченный FXML
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/Menu.fxml")));
+        stage.setScene(new Scene(root));
         stage.setMaxHeight(1000);
         stage.setMaxWidth(700);
         stage.setWidth(700);
